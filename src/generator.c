8fc2d1fee8cd3c860053e234f93846a588e43846
Linter <linter@mail.com>
2022-11-23 11:14:28 +0000

    lint source

--- src/generator.c
+++ src/generator.c
@@ -2,29 +2,23 @@
 
 int rCnt = 0;
 
-int getNewRegister() {
-    return rCnt++;
-}
+int getNewRegister() { return rCnt++; }
 
-void printRegister(int r) {
-    fprintf(listing, "r%d", r);
-}
+void printRegister(int r) { fprintf(listing, "r%d", r); }
 
-void printChar(char c) {
-    fprintf(listing, "%c", c);
-}
+void printChar(char c) { fprintf(listing, "%c", c); }
 
 void generateTree(TreeNode *t) {
     if (t == NULL) {
         return;
     }
     switch (t->nodekind) {
-        case StmtK:
-            break;
-        case ExpK:
-            break;
-        case DeclK:
-            break;
+    case StmtK:
+        break;
+    case ExpK:
+        break;
+    case DeclK:
+        break;
     }
 }
 
@@ -33,14 +27,14 @@ void generateStatement(TreeNode *t) {
         return;
     }
     switch (t->kind.exp) {
-        case IfK:
-            break;
-        case RepeatK:
-            break;
-        case AssignK:
-            break;
-        case ReturnK:
-            break;
+    case IfK:
+        break;
+    case RepeatK:
+        break;
+    case AssignK:
+        break;
+    case ReturnK:
+        break;
     }
 }
 
@@ -49,26 +43,26 @@ void generateExpression(TreeNode *t, int finalRegister) {
         return;
     }
     switch (t->kind.exp) {
-        case OpK:
-            int left = getNewRegister(), right = getNewRegister();
-            generateExpression(t->child[0], left);
-            generateExpression(t->child[0], right);
-            printRegister(finalRegister);
-            fprintf(listing, " = ");
-            printRegister(left);
-            printChar(' ');
-            printOp(t->attr.op);
-            printChar(' ');
-            printRegister(right);
-            printChar('\n');
-            break;
-        case ConstK:
-            printRegister(finalRegister);
-            fprintf(listing, " = ");
-            fprintf(listing, "%d", t->attr.val);
-            break;
-        case IdK:
-            if ()
+    case OpK:
+        int left = getNewRegister(), right = getNewRegister();
+        generateExpression(t->child[0], left);
+        generateExpression(t->child[0], right);
+        printRegister(finalRegister);
+        fprintf(listing, " = ");
+        printRegister(left);
+        printChar(' ');
+        printOp(t->attr.op);
+        printChar(' ');
+        printRegister(right);
+        printChar('\n');
+        break;
+    case ConstK:
+        printRegister(finalRegister);
+        fprintf(listing, " = ");
+        fprintf(listing, "%d", t->attr.val);
+        break;
+    case IdK:
+        if ()
             break;
     }
 }
@@ -78,18 +72,16 @@ void generateDeclaration(TreeNode *t) {
         return;
     }
     switch (t->kind.exp) {
-        case VarK:
-        case VecK:
-        case IdK:
-            break;
+    case VarK:
+    case VecK:
+    case IdK:
+        break;
     }
 }
 
-void generateVector(TreeNode *t){
-    int registerName = getNewRegister()
-    printRegister(registerName);
-    fprintf(listing, " = %d*4\n",t->child[1]);
+void generateVector(TreeNode *t) {
+    int registerName = getNewRegister() printRegister(registerName);
+    fprintf(listing, " = %d*4\n", t->child[1]);
 
-    fprintf(listing, "%s = %s[%s]\n",t->child[0]);
+    fprintf(listing, "%s = %s[%s]\n", t->child[0]);
 }
-
